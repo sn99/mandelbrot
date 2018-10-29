a19b5dbe3e37826d34294edbfc37721db5701d15
sn99 <siddharthn.099@gmail.com>
2018-10-30 01:39:57 +0530

    cargo fmt && cargo fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-extern crate num;
-extern crate image;
 extern crate crossbeam;
+extern crate image;
+extern crate num;
 
-use num::Complex;
 use image::{png::PNGEncoder, ColorType};
+use num::Complex;
 
 use std::{fs::File, io::Write, str::FromStr};
 
@@ -115,19 +115,19 @@ fn main() {
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
     let threads = 8;
-    let rows_per_band = bounds.1 / threads +1;
+    let rows_per_band = bounds.1 / threads + 1;
 
     {
         let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
 
-        crossbeam::scope(|spawner|{
-            for (i, band) in bands.into_iter().enumerate(){
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
                 let top = rows_per_band * i;
                 let height = band.len() / bounds.0;
                 let band_bounds = (bounds.0, height);
                 let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
-                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
-
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
 
                 spawner.spawn(move || {
                     render(band, band_bounds, band_upper_left, band_lower_right);
